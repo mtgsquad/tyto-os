b66e9477c6b767842e2ac54a32b2e486d22270f4
Jay Jackson <jj@jj.is-a.dev>
2022-03-30 22:32:39 -0400

    grr

--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,27 @@
 #![feature(abi_x86_interrupt)]
 
 use bootloader::{entry_point, BootInfo};
-use core::panic::PanicInfo;
+use core::{fmt::Write, panic::PanicInfo};
+use vga::colors::Color16;
+use vga::writers::{Graphics640x480x16, GraphicsWriter};
 
 pub mod interrupts;
 pub mod serial;
 
 entry_point!(kernel_main);
 
-fn kernel_main(boot_info: &'static mut BootInfo) -> ! {
-    if let Some(framebuffer) = boot_info.framebuffer.as_mut() {
-        let info = framebuffer.info();
-
-        serial_println!("{}", framebuffer.buffer().len());
-
-        for y in 0..info.vertical_resolution {
-            for x in 0..info.horizontal_resolution {
-                let byte = &mut framebuffer.buffer_mut()[((y + x) * info.horizontal_resolution)];
-
-                *byte = 0xFF;
-            }
-        }
-    }
+fn kernel_main(_boot_info: &'static mut BootInfo) -> ! {
+    // let mode = Graphics640x480x16::new();
+    // mode.set_mode();
+    // mode.clear_screen(Color16::Black);
+    // mode.draw_line((80, 60), (80, 420), Color16::White);
+    // mode.draw_line((80, 60), (540, 60), Color16::White);
+    // mode.draw_line((80, 420), (540, 420), Color16::White);
+    // mode.draw_line((540, 420), (540, 60), Color16::White);
+    // mode.draw_line((80, 90), (540, 90), Color16::White);
+    // for (offset, character) in "Hello World!".chars().enumerate() {
+    //     mode.draw_character(270 + offset * 8, 72, character, Color16::White)
+    // }
 
     #[allow(clippy::empty_loop)]
     loop {}
@@ -31,6 +31,12 @@ fn kernel_main(boot_info: &'static mut BootInfo) -> ! {
 
 /// This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    {
+        let mut serial_port = unsafe { uart_16550::SerialPort::new(0x3F8) };
+        serial_port.init();
+        write!(serial_port, "Kernel panic: {}", info).expect("Printing to serial failed");
+    }
+
     loop {}
 }
