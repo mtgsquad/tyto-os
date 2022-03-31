7d68aab0e6896462b88f2ffd2a7b3f4e4e5240c1
Jay Jackson <jj@jj.is-a.dev>
2022-03-31 10:59:46 -0400

    HOW IS IT GREEN????/

--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,23 @@
 
 use bootloader::{entry_point, BootInfo};
 use core::{fmt::Write, panic::PanicInfo};
-use vga::colors::Color16;
-use vga::writers::{Graphics640x480x16, GraphicsWriter};
 
 pub mod interrupts;
 pub mod serial;
 
 entry_point!(kernel_main);
 
-fn kernel_main(_boot_info: &'static mut BootInfo) -> ! {
-    let mode = Graphics640x480x16::new();
-    mode.set_mode();
-    // mode.clear_screen(Color16::Black);
-    // mode.draw_line((80, 60), (80, 420), Color16::White);
-    // mode.draw_line((80, 60), (540, 60), Color16::White);
-    // mode.draw_line((80, 420), (540, 420), Color16::White);
-    // mode.draw_line((540, 420), (540, 60), Color16::White);
-    // mode.draw_line((80, 90), (540, 90), Color16::White);
-    // for (offset, character) in "Hello World!".chars().enumerate() {
-    //     mode.draw_character(270 + offset * 8, 72, character, Color16::White)
-    // }
+fn kernel_main(boot_info: &'static mut BootInfo) -> ! {
+    if let Some(framebuffer) = boot_info.framebuffer.as_mut() {
+        let info = framebuffer.info();
+
+        for (i, byte) in framebuffer.buffer_mut().iter_mut().enumerate() {
+            let y = i % info.horizontal_resolution;
+            let x = i % info.vertical_resolution;
+
+            *byte = if x % 2 == 0 { 0x00 } else { 0xFF };
+        }
+    }
 
     #[allow(clippy::empty_loop)]
     loop {}
