2cd69f2cf171503280e79890b2ac2c60ad87e911
Jay Jackson <jj@jj.is-a.dev>
2022-04-08 12:59:19 -0400

    going to work on keyboard drivers

--- src/main.rs
+++ src/main.rs
@@ -2,29 +2,27 @@
 #![no_std]
 #![feature(abi_efiapi)]
 
+pub mod device;
+
 use uefi::{prelude::*, proto::console::gop::GraphicsOutput};
 
 #[entry]
 fn main(image: Handle, mut system_table: SystemTable<Boot>) -> Status {
     uefi_services::init(&mut system_table).unwrap();
 
-    let mut framebuffer = unsafe {
-        if let Ok(gop) = system_table
-            .boot_services()
-            .locate_protocol::<GraphicsOutput>()
-        {
-            &mut *gop.get()
-        } else {
-            return Status::NO_MEDIA;
-        }
+    let mut framebuffer = if let Ok(gop) = system_table
+        .boot_services()
+        .locate_protocol::<GraphicsOutput>()
+    {
+        unsafe { &mut *gop.get() }
+    } else {
+        return Status::NO_MEDIA;
     }
     .frame_buffer();
 
     // clear the screen
     for i in 0..framebuffer.size() {
-        unsafe {
-            framebuffer.write_value(i, 0x00)
-        }
+        unsafe { framebuffer.write_value(i, 0x00) }
     }
 
     Status::SUCCESS
